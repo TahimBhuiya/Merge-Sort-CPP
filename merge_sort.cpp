35a4e163d5a355c8ff4bd1ec2c4f53a57e0e4347
Tahim Bhuiya <tbhuiya2018@fau.edu>
2025-09-20 19:42:10 -0400

    Added onto merge function

--- merge_sort.cpp
+++ merge_sort.cpp
@@ -6,4 +6,6 @@ void merge(int arr[], int left, int mid, int right) {
     // Find the sizes of two subarrays to merge
     int n1 = mid - left + 1; // size of left subarray
     int n2 = right - mid;    // size of right subarray
+
+    int *L = new int[n1];
 }
