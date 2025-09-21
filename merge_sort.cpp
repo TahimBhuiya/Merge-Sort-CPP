dc01a4b9bca19161a1ef02b183792852dc69d492
Tahim Bhuiya <tbhuiya2018@fau.edu>
2025-09-21 18:58:55 -0400

    Added onto merge function

--- merge_sort.cpp
+++ merge_sort.cpp
@@ -11,6 +11,8 @@ void merge(int arr[], int left, int mid, int right) {
     int *L = new int[n1];
     int *R = new int[n2];
 
+    
+    // Copy data to temporary arrays
     for (int i = 0; i < n1; i++)
         L[i] = arr[left + i];
     for (int j = 0; j < n2; j++)
